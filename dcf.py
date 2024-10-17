f44a84c3470cd65ba83e5b703e44be3e3f443335
EDRV02 <eduardo.diazriovarez@hotmail.com>
2024-10-17 12:01:53 +0200

    deleted dummy files

--- dcf.py
+++ dcf.py
@@ -1,39 +1,16 @@
-import pandas as pd
-import numpy as np
 import yfinance as yf
-import seaborn as sns
-import matplotlib.pyplot as plt
-data = yf.download('MSFT', '2018-01-01', '2023-01-01')
+import plotly.graph_objects as go
 
-# Define long and short window
-short_window = 50
-long_window = 200
+# Download historical stock data for Apple (AAPL)
+stock_data = yf.download('AAPL', start='2020-01-01', end='2024-08-01')
 
-# Calculate moving averages
-data['short_mavg'] = data['Close'].rolling(short_window).mean()
-data['long_mavg'] = data['Close'].rolling(long_window).mean()
+# Plotting the candlestick chart with Plotly
+fig = go.Figure(data=[go.Candlestick(x=stock_data.index,
+                                     open=stock_data['Open'],
+                                     high=stock_data['High'],
+                                     low=stock_data['Low'],
+                                     close=stock_data['Close'])])
+fig.update_layout(title='AAPL Candlestick Chart', xaxis_title='Date', yaxis_title='Price')
 
-# Take long positions
-data['long_positions'] = np.where(data['short_mavg'] > data['long_mavg'], 1, 0)
-
-# Take short positions
-data['short_positions'] = np.where(data['short_mavg'] < data['long_mavg'], -1, 0)
-data['positions'] = data['long_positions'] + data['short_positions']
-
-# Plot close price and moving averages
-plot_data = data[-3000:]
-plt.figure(figsize=(10, 7))
-plt.title('Long and Short Signal', fontsize=14)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.plot(plot_data['Close'], label='Close')
-plt.plot(plot_data['short_mavg'], label='50-Day Moving Average')
-plt.plot(plot_data['long_mavg'], label='200-day Moving Average')
-plt.plot(plot_data[(plot_data['long_positions'] == 1) &
-(plot_data['long_positions'].shift(1) == 0)]['short_mavg'],
-'^', ms=15, label='Buy Signal', color='green')
-plt.plot(plot_data[(plot_data['short_positions'] == -1) &
-(plot_data['short_positions'].shift(1) == 0)]['short_mavg'],
-'^', ms=15, label='Sell Signal', color='red')
-plt.legend()
-plt.show()
+# Display the figure
+fig.show()
